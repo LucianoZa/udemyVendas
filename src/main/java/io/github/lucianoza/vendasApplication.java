670fb009646bdbb7e1a563150192d1e948e981fd
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-07 12:33:48 -0300

    Hello World via WEB

--- src/main/java/io/github/lucianoza/vendasApplication.java
+++ src/main/java/io/github/lucianoza/vendasApplication.java
@@ -2,12 +2,19 @@ package io.github.lucianoza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController //veio com spring-boot-starter-web
 public class vendasApplication {
+
+    @GetMapping("/hello") // acessar em http://localhost:8080/hello
+    public String HelloWorld(){
+        return "Hello World!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(vendasApplication.class, args);
-        System.out.println("Hello World!");
-
+        //System.out.println("Hello World!");
     }
 }
