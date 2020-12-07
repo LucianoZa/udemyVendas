3f342dc5c147e64f8b9c7f0aeef6d802b8da5ff7
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-07 11:58:43 -0300

    Inicio - Hello World no console

---
+++ src/main/java/io/github/lucianoza/vendasApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucianoza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class vendasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(vendasApplication.class, args);
+        System.out.println("Hello World!");
+
+    }
+}
