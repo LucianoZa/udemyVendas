f6d61a2807d5efe7a83f90a4e1276ae0502037d0
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-07 19:48:31 -0300

    Injeção no construtor, @Autowired  é opcional, pois Spring subentende

--- src/main/java/io/github/lucianoza/service/ClienteService.java
+++ src/main/java/io/github/lucianoza/service/ClienteService.java
@@ -11,17 +11,18 @@ public class ClienteService {
 //    @Autowired //    Injeção no própria Declaração
 //    private ClienteRepository repository;
 
-    private ClienteRepository repository;
-    @Autowired //    Injeção via set // basta digitar set que a IDE sugere
-    public void setRepository(ClienteRepository repository) {
-        this.repository = repository;
-    }
-
-//    Injeção no próprio construtor = melhor opção e até para testes unitários
 //    private ClienteRepository repository;
-//    public ClienteService(ClienteRepository repository) {
+//    @Autowired //    Injeção via set // basta digitar set que a IDE sugere
+//    public void setRepository(ClienteRepository repository) {
 //        this.repository = repository;
 //    }
+
+    //Injeção no próprio construtor = melhor opção e até para testes unitários
+    //@Autowired => Injeção no construtor é opcional, pois Spring subentende
+    private ClienteRepository repository;
+    public ClienteService(ClienteRepository repository) {
+        this.repository = repository;
+    }
 
     public void salvarCliente(Cliente cliente) {
         validarCliente(cliente);
