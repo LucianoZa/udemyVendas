d8f96ae772af8481c5439a76efd2ff3eaae5fb15
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-08 09:37:15 -0300

    Configuração e obtendo conexões com bases de dados h2

--- src/main/java/io/github/lucianoza/vendasApplication.java
+++ src/main/java/io/github/lucianoza/vendasApplication.java
@@ -1,28 +1,12 @@
 package io.github.lucianoza;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController //veio com spring-boot-starter-web
 public class vendasApplication {
 
-    @Value("${application.name}") //chave tem que estar em resources\application.properties !
-    private String applicationName;
-
-    @GetMapping("/hello") // acessar em http://localhost:8080/hello
-    public String HelloWorld(){
-        return applicationName;
-    }
     public static void main(String[] args) {
         SpringApplication.run(vendasApplication.class, args);
-        //System.out.println("Hello World!");
     }
 }
