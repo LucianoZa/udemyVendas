beb675f23c4077de431c609a63b5646a9aea8e96
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-08 11:53:52 -0300

    Criando Classes do Modelo de dados

---
+++ src/main/java/io/github/lucianoza/domain/entity/Cliente.java
@@ -0,0 +1,23 @@
+package io.github.lucianoza.domain.entity;
+
+public class Cliente {
+
+    private String id;
+    private String nome;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
