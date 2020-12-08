f29d2c4825041fd586392414b218277a1c0e07da
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-08 16:00:09 -0300

    Concluindo operações do cliente: Delete, Update, busca por nome like

--- src/main/java/io/github/lucianoza/domain/repository/Clientes.java
+++ src/main/java/io/github/lucianoza/domain/repository/Clientes.java
@@ -14,6 +14,8 @@ import java.util.List;
 public class Clientes {
 
     private static String INSERT = "insert into cliente (nome) values (?) ";
+    private static String UPDATE = "update cliente set nome = ? where id = ? ";
+    private static String DELETE = "delete cliente where id = ? ";
     private static String SELECT_ALL = "select * from cliente ";
 
     @Autowired
@@ -24,8 +26,32 @@ public class Clientes {
         return cliente;
     }
 
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(
+                SELECT_ALL.concat(" where nome like ?"),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+    }
+
     public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
@@ -33,6 +59,6 @@ public class Clientes {
 
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 }
