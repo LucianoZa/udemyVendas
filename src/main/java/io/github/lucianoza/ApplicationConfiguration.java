bc783ba0698e2da02f10f27e0b9881b4090d1313
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-07 21:01:41 -0300

    multiplos ambientes e profiles (propriedades e configurações)

--- src/main/java/io/github/lucianoza/ApplicationConfiguration.java
+++ src/main/java/io/github/lucianoza/ApplicationConfiguration.java
@@ -1,18 +1,36 @@
 package io.github.lucianoza;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration //Marca para Spring scanear esta classe
+
 public class ApplicationConfiguration {
-
-    @Bean(name = "applicationName") //Se não passar o name ele pega o nome do método por default
-    public String applicationName(){
-        return "Sistema de Vendas 1";
+    @Profile("development") //Indica que só vai funcionar para o contexto especificado
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando DEV");
+        };
     }
 
-    @Bean(name = "outroName")
-    public String outroName(){
-        return "Sistema de Vendas 2";
+    @Profile("production") //Indica que só vai funcionar para o contexto especificado
+    @Bean
+    public CommandLineRunner executar2(){
+        return args -> {
+            System.out.println("Rodando PRD");
+        };
     }
+
+//    @Bean(name = "applicationName") //Se não passar o name ele pega o nome do método por default
+//    public String applicationName(){
+//        return "Sistema de Vendas 1";
+//    }
+//
+//    @Bean(name = "outroName")
+//    public String outroName(){
+//        return "Sistema de Vendas 2";
+//    }
 }
