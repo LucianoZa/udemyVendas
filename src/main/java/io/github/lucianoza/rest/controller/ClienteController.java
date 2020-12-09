ef257adbe112e7ea19ec40bae93f10512092e9af
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-09 18:35:04 -0300

    Outras opções de Request Mapping

--- src/main/java/io/github/lucianoza/rest/controller/ClienteController.java
+++ src/main/java/io/github/lucianoza/rest/controller/ClienteController.java
@@ -1,13 +1,28 @@
 package io.github.lucianoza.rest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.lucianoza.domain.entity.Cliente;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // ou @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
-    @RequestMapping("/hello")
-    public String helloCliente(String nomeCliente) {
+
+    @RequestMapping(
+            value = "/hello/{nome}", //Opcional: {"/hello/{nome}", "/api/hello", "/api/etc."}
+            method = RequestMethod.GET//,
+            //consumes = {"application/json", "application/xml"},  //minetype = o que eu aceito receber!
+            //produces = {"application/json", "application/xml"}  // indica formato de saida/retorno do servico
+    )
+    @ResponseBody // Indicar que o retorno será no Corpo da pagina!
+    //Se retornar Objeto Cliente formato indicado por produces!
+//            method = RequestMethod.POST,
+//    public Cliente helloCliente(@PathVariable("nome") String nomeCliente
+//            , @RequestBody Cliente cliente //associado consumes acima, indica formato objeto parametro esperado
+//    ) {
+//        return cliente;
+//    }
+// Se retornar String
+    public String helloCliente(@PathVariable("nome") String nomeCliente ) {
         return String.format("Hello %s ", nomeCliente);
     }
 }
