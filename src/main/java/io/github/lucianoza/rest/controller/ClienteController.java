17b1b82bfe555cb2ce8198654625c7e7ee90b529
luciano_zanolete <luciano_zanolete@terceiros.sicredi.com.br>
2020-12-10 12:09:35 -0300

    REST UPDATE Cliente

--- src/main/java/io/github/lucianoza/rest/controller/ClienteController.java
+++ src/main/java/io/github/lucianoza/rest/controller/ClienteController.java
@@ -48,4 +48,17 @@ public class ClienteController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id,
+                                 @RequestBody Cliente cliente) {
+        return clientes.findById(id)
+                .map((clienteExistente) ->{
+                    cliente.setId(clienteExistente.getId());
+                    clientes.save(cliente);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
